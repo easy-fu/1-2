600bfa178107830357b29c30de55ba1383182bd7
easy-fu <2416740760@qq.com>
2020-10-17 09:58:06 +0800

    .

---
+++ 1-2/1-2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace cv;
+
+int main()
+
+{
+	cv::Mat srcMat = imread("C:/Users/DELL/Desktop/1.jpg", 0);
+	imshow("src", srcMat);
+	waitKey(0);
+	return 0;
+}
